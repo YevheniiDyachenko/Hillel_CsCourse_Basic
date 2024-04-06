2151ceb42f8f4f671722fd87573f74333b67cfed
Yevhenii Diachenko <ukrmail.des@gmail.com>
2024-04-06 20:06:08 +0300

    added scond task

---
+++ Lesson 2/2.2 Time in seconds/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2._2_Time_in_seconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
