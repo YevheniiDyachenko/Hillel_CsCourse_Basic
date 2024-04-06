22378924a23cde9e015e5bb3e98194314e3ed188
Yevhenii Diachenko <ukrmail.des@gmail.com>
2024-04-06 19:43:57 +0300

    added 2.1 HW

---
+++ 2.1 Discount Amount/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2._1_Discount_Amount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
