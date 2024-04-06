036edf05af0cdff475bc53e8c491438b592912e9
Yevhenii Diachenko <ukrmail.des@gmail.com>
2024-04-06 21:07:35 +0300

    Made 1st task

--- Lesson 2/2.1 Discount Amount/Program.cs
+++ Lesson 2/2.1 Discount Amount/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            decimal price = 100;
+            byte discount = 49;
+            
+            Console.WriteLine("Enter the item price (press Enter to use default price):");
+            string input = Console.ReadLine();
+
+            if (input != "") // when user click Enter , then use base value for price 
+            {
+                price = Convert.ToDecimal(input);
+            }
+
+            decimal totalPrice = price * discount / 100;
+
+            Console.WriteLine($" Item price: {price} UAH \n Discunt: {discount}% \n Total price:{totalPrice} UAH !!!");
+            Console.ReadLine();
         }
     }
 }
