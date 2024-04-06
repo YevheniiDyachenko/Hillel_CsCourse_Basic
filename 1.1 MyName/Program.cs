8fc89ab2ea333bcc55cf5801d9708607f2dbffb3
Yevhenii Diachenko <ukrmail.des@gmail.com>
2024-04-06 19:32:55 +0300

    Добавьте файлы проекта.

---
+++ 1.1 MyName/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _1._1_MyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            string textStart = "Мене звуть ";
+            string textEnd = "Я навчаюсь у школі Hillel";
+            string name = "Євгеній";
+            Console.WriteLine ($"{textStart} {name}");
+            Console.WriteLine ($"{textEnd}");
+            Console.Read();
+        }
+    }
+}
---
+++ Time in a second/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Time_in_a_second
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
