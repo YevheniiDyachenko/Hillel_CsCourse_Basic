1f8c40dfd9ab92f969ac8a844f04be324e8cea3a
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-04-09 21:54:46 +0300

    Added result for the 2d task

--- Lesson 3/3.2 Calculates the square of any given number/Program.cs
+++ Lesson 3/3.2 Calculates the square of any given number/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace _3._2_Calculates_the_square_of_any_given_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a number from the user.
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            // 1 Variant
+            // Calculate the square of a given number.
+            int square = number * number;
+            
+            // Display the square of a given number.
+            Console.WriteLine($"The square of {number} is {square}.");
+            
+            // 2 Variant
+            // Calculate the square of a given number with Math class.
+            int square2 = (int)Math.Pow(number, 2);
+            
+            // Display the square of a given number.
+            Console.WriteLine($"The square of {number} is {square2}.");
+            
+            Console.ReadLine();
+        }
+    }
+}            
