2cac7566120dabe3eee4c381a6fcac49c05c8e5b
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-04-09 22:29:06 +0300

    5 task is done

--- Lesson 3/3.5 Сircle area/Program.cs
+++ Lesson 3/3.5 Сircle area/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace _3._5_Сircle_area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a radius from the user.
+            Console.WriteLine("Enter a radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            
+            // Calculate the area of a circle.
+            double area = Math.PI * Math.Pow(radius,2 );
+            
+            // Display the area of a circle.
+            Console.WriteLine($"The area of a circle with radius {radius} is {area}.");
+            
+            Console.ReadLine();
+        }
+    }
+}
