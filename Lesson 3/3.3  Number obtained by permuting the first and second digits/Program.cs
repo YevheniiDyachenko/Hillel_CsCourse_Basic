0d5d28cb1fca3c5b294e042246cf848d3d26fe04
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-04-09 22:03:17 +0300

    Done 3d task

--- Lesson 3/3.3  Number obtained by permuting the first and second digits/Program.cs
+++ Lesson 3/3.3  Number obtained by permuting the first and second digits/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace _3._3__Number_obtained_by_permuting_the_first_and_second_digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a three digit number from the user.
+            Console.WriteLine("Enter a three digit number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            // 1 Variant
+            // Calculate the resulted number.
+            int reversedNumber =  number/100*10 + (number/10)%10*100+ number % 10;
+            
+            // Display the resulted number.
+            Console.WriteLine($"The Resulted Number is: {reversedNumber}.");
+            
+            // 2 Variant
+            // Get the digits from right to left.
+            int firstDigit = number / 100;
+            int secondDigit = (number % 100) / 10;
+            int thirdDigit = number % 10;
+            
+            // Display the digits.
+            Console.WriteLine($"The result of permuting the first and second digits is: {secondDigit}{firstDigit}{thirdDigit}.");
+            
+            Console.ReadLine();
+        }
+    }
+}
