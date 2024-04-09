c9000ec41a8d285e507b73ae75920d1ba3051ec0
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-04-09 22:19:48 +0300

    done 4 task

--- Lesson 3/3.4 Arithmetic average of two numbers/Program.cs
+++ Lesson 3/3.4 Arithmetic average of two numbers/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace _3._4_Arithmetic_average_of_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a numbers from the user.
+            Console.WriteLine("Enter the first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter the second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            
+            // Calculate the average of two numbers.
+            double average = (firstNumber + secondNumber)/2;
+            
+            // Display the average of two numbers.
+            Console.WriteLine($"The average of {firstNumber} and {secondNumber} is {average}.");
+            
+            Console.ReadLine();
+        }
+    }
+}
