dd1a54239959f3b45fd6956c446e2eeaf18c19c8
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-04-09 21:39:10 +0300

    Create 2 variant for the 1 task with 3 digit number

--- Lesson 3/3.1 From right to left digits/Program.cs
+++ Lesson 3/3.1 From right to left digits/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿
+namespace _3._1_From_right_to_left_digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a three digit number from the user.
+            Console.WriteLine("Enter a three digit number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            // 1 Variant
+            // Calculate the reversed number.
+            int reversedNumber =  number/100 + (number/10)%10*10 + number % 10*100;
+            
+            // Display the reversed number.
+            Console.WriteLine($"The Reversed Number is: {reversedNumber}.");
+            
+            // 2 Variant
+            // Get the digits from right to left.
+            int firstDigit = number / 100;
+            int secondDigit = (number % 100) / 10;
+            int thirdDigit = number % 10;
+            
+            // Display the digits from right to left.
+            Console.WriteLine($"The digits from right to left are: {thirdDigit}{secondDigit}{firstDigit}.");
+            
+            Console.ReadLine();
+        }
+    }
+}
