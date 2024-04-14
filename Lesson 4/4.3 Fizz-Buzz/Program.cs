d9ebc0e8c6018b712074e4d2fc9ee52abff1ef71
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-04-14 20:16:37 +0300

    4.3 is finished

---
+++ Lesson 4/4.3 Fizz-Buzz/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _4._3_Fizz_Buzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a number from the user.
+            Console.WriteLine("Enter a number from 1 to 100: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //Solution using a ternary operator
+            Console.WriteLine((number % 3 == 0 ? "Fizz" : "") + (number % 5 == 0 ? "Buzz" : ""));
+            Console.ReadLine();
+        }
+    }
+}
