2257565e92d170869f8e19c4d533818584dcc8ae
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-04-16 21:43:30 +0300

    5.1 TASK DONE

---
+++ Lesson 5/5.1 Correct Password/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _5._1_Correct_Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? password;
+            do
+            {
+                // Get the password from the user.
+                Console.WriteLine("Enter the password:");
+                password = Console.ReadLine();
+
+                // Check if the password is correct.
+                if (password != "root")
+                {
+                    // If the password is incorrect, display an error message.
+                    Console.WriteLine("Wrong password!");
+                }
+            } while (password != "root");
+
+            // If the password is correct, display a success message.
+            Console.WriteLine("Correct password!");
+        }
+    }
+}
