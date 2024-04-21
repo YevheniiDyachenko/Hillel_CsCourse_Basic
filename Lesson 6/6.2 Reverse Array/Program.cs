2232e0c4768314ff3f2435d8b18d19e44a6ba93e
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-04-21 19:18:38 +0300

    added new variant for 6.2 task

--- Lesson 6/6.2 Reverse Array/Program.cs
+++ Lesson 6/6.2 Reverse Array/Program.cs
@@ -15,9 +15,22 @@
             ResultArray(arr);
             Console.WriteLine();
 
-            //Display Reversed array
-            ReverseArray(arr);
-            Console.WriteLine("The reversed array:");
+            // Display 1 variant Reversed array (using new array)
+            int[] newArray = ReverseArrayNewArray(arr);
+            Console.WriteLine("The reversed array (using New Array) :");
+            ResultArray(newArray);
+            Console.WriteLine();
+            
+            //Display 2 variant Reversed array (using Array.Reverse)
+            ReverseArrayReverse(arr);
+            Console.WriteLine("The reversed array (using Array.Reverse):");
+            ResultArray(arr);
+            Console.WriteLine();
+            
+            //Display 3 variant Reversed array (using temp variable)
+            ReverseArrayTemp(arr);
+            ReverseArrayTemp(arr); //used twice to reverse array, because it was reversed before by ReverseArrayReverse method
+            Console.WriteLine("The reversed array (using temp variable):");
             ResultArray(arr);
             Console.ReadLine();
         }
@@ -31,11 +44,42 @@
                 arr[i] = random.Next(-100, 101);
             }
         }
-
-        //Reverse the array
-        private static void ReverseArray(int[] arr)
+        
+        //Reverse the array (using new array)
+        private static int [] ReverseArrayNewArray(int[] arr)
+        {
+            int[] reversedArray = new int[arr.Length];
+            
+            int index = arr.Length;
+            foreach (int i in arr)
+            {
+                reversedArray[index-1] = i;
+                index--;
+            }
+            
+            return reversedArray;
+        }
+        
+        //Reverse the array (using Array.Reverse)
+        private static void ReverseArrayReverse(int[] arr)
         {
             Array.Reverse(arr);
+        }
+        
+        //Reverse the array (using temp variable)
+        private static void ReverseArrayTemp(int[] arr)
+        {
+            int min = 0;
+            int max = arr.Length - 1;
+            int temporary;
+            while (min < max)
+            {
+                temporary = arr[min];
+                arr[min] = arr[max];
+                arr[max] = temporary;
+                min++;
+                max--;
+            }
         }
 
         // Display array elements
