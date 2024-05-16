a9c1c7e0065fd49a386f565e2719932f6e778e36
Yevhenii Diachenko <87785680+YevheniiDyachenko@users.noreply.github.com>
2024-05-16 16:22:19 +0300

    11.2 done

---
+++ Lesson 11/11.2 AgeInputExceptionHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _11._2_AgeInputExceptionHandling
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter your age: ");
+                    string input = Console.ReadLine();
+                    
+                    if (!int.TryParse(input, out int age))
+                    {
+                        throw new FormatException("The age must be an integer.");
+                    }
+
+                    if (age < 0)
+                    {
+                        throw new FormatException("The age cannot be less than 0.");
+                    }
+
+                    Console.WriteLine($"Your age: {age}");
+                    break; 
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"Format error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
